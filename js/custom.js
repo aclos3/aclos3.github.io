6c41abeb4c0f1230d1c54ecbc16c87a0cf547bdc
aclos3 <45724446+aclos3@users.noreply.github.com>
2020-01-17 16:44:01 -0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -213,13 +213,13 @@
 
 	// Page Nav
 	var clickMenu = function() {
-
 		$('.navbar-nav a:not([class="external"])').click(function(event){
-
+			
 			var section = $(this).data('nav-section'),
 				navbar = $('.navbar-nav');
 				if (isMobile.any()) {
 					$('.navbar-toggle').click();
+				
 				}
 				if ( $('[data-section="' + section + '"]').length ) {
 			    	$('html, body').animate({
